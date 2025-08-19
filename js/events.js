6b647e41e421a9411283bcedfff5af5f3d81009b
Anonnya01 <anonnya1999@gmail.com>
2025-08-19 19:56:30 +0600

    button with event handlers

--- js/events.js
+++ js/events.js
@@ -1,1 +1,24 @@
 console.log('external file');
+ // <!-- option 2 function -->
+        function makeYellow() {
+          document.body.style.backgroundColor = "yellow";
+        }
+        function makePink() {
+          document.body.style.backgroundColor = "plum";
+        }
+
+
+         // option 3
+
+      const btnGreen = document.getElementById("btn-green");
+      btnGreen.onclick = function green() {
+        document.body.style.backgroundColor = "lightgreen";
+      };
+
+       // optional--sky
+      const btnSky = document.getElementById('btn-sky');
+      btnSky.onclick =skyBlue
+       function skyBlue() {
+        document.body.style.backgroundColor ='skyblue'
+       }
+     
